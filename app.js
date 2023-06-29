d0ce228649d1210927d4111c0ec99bdf48170041
dianaaggar <57739072+dianaaggar@users.noreply.github.com>
2023-06-29 11:59:11 -0400

    update style css

--- app.js
+++ app.js
@@ -15,4 +15,36 @@ let blocks = Array.from(blocksContainer.children);
 console.log(blocks.length);
 
 let orderRange = [...Array(blocks.length).keys()];
+//let orderRange = Array.from(Array(blocks.length).keys());
 console.log(orderRange);
+
+let testOrderRange = [1, 11, 13, 18, 17, 19, 0, 2, 16, 5, 7, 9, 3, 10, 4, 6, 8, 14, 15, 12];
+
+blocks.forEach((block, index) => {
+    block.style.order = index;
+
+    block.style.order = testOrderRange[index];
+});
+// shufle function
+function shuffle(array) {
+    // settings vars
+    let current = array.length
+    temp,
+    random;
+
+    while (current > 0) {
+        // get random number
+        random = Math.floor(Math.random() * current);
+
+    // decrease length by one
+    current--;
+
+    // 1-save current element in strach
+    temp = array[current];
+    // 2- current element = random element
+    array[current] = array[random];
+    //3- random element = get element from stash
+    array[random] = temp;
+    }
+    return array;
+}
